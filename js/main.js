6c0c617c0585d923a8fa2cfd99e035929bb682bd
MBronz <m.bronz@outlook.sa>
2017-04-26 00:36:11 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(" .home").vegas({
+    slides: [
+        { src: "img/samuel-zeller-118195.jpg" },
+        { src: "img/freestocks-org-126848.jpg" },
+        { src: "img/daniel-frank-201417.jpg" },
+        { src: "img/OGMQ8I0.jpg" }
+    ],
+     
+});
+AOS.init({
+  duration: 1200,
+})
